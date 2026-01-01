0b47cc8fb7bb54523f9c57e85adcd8580017ed4f
Jason Seawall <jseawall@numurus.com>
2026-01-01 11:21:32 -0800

    **

--- ai_train_project_setup.sh
+++ ai_train_project_setup.sh
@@ -19,19 +19,19 @@ else
   PROJECT_NAME=yolo_train_project
 fi
 
-PROJECT_FOLDER="$(dirname $(dirname $(pwd)))/${PROJECT_NAME}"
+PROJECT_FOLDER="$(dirname $(pwd))/${PROJECT_NAME}"
 
 if [[ -d "$PROJECT_FOLDER" ]]; then
   echo "Project folder allready exists at ${PROJECT_FOLDER}, will update process scripts"
   RSYNC_EXCLUDES=" --exclude project_settings.yaml"
   echo "Excluding ${RSYNC_EXCLUDES}"
-  rsync -arh ${RSYNC_EXCLUDES} $(pwd)/* ${PROJECT_FOLDER}/
-  sudo cp -r $(dirname $(pwd))/src/nepi_ai_training/* ${PROJECT_FOLDER}/
+  rsync -arh ${RSYNC_EXCLUDES} $(pwd)/nepi_yolo_detector_training/* ${PROJECT_FOLDER}/
+  sudo cp -r $(pwd)/src/nepi_ai_training/* ${PROJECT_FOLDER}/
 
 else
   sudo mkdir $PROJECT_FOLDER
-  sudo cp -r $(pwd)/* ${PROJECT_FOLDER}/
-  sudo cp -r $(dirname $(pwd))/src/nepi_ai_training/* ${PROJECT_FOLDER}/
+  sudo cp -r $(pwd)/nepi_yolo_detector_training/* ${PROJECT_FOLDER}/
+  sudo cp -r $(pwd)/src/nepi_ai_training/* ${PROJECT_FOLDER}/
   sudo chown -R ${USER}:${USER} $PROJECT_FOLDER
   sudo chmod -R +x $PROJECT_FOLDER
   echo "Project folder created at ${PROJECT_FOLDER}"
